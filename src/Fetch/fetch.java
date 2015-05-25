b9203f9bc1f01141d55e5f8fb58643156663f323
Fabian Fdz <fa.fdz@me.com>
2015-05-24 19:33:02 -0600

    Se agregó el fetch por demanda

--- src/Fetch/fetch.java
+++ src/Fetch/fetch.java
@@ -1,6 +1,9 @@
 package Fetch;
 
 import Constructores.Atributos_text;
+import Constructores.Configuracion;
+import Constructores.Constantes;
+import Constructores.Pagina;
 import Constructores.Proceso;
 
 /**
@@ -15,6 +18,18 @@ public class fetch {
 
     public static void fetch_demanda(Atributos_text linea, String tipo_reemplazo) {
         //cada vez que hace una referencia pasa la pagina a memoria
+        Pagina new_pag = new Pagina(-1, -1, null);
+        for (int i = 0; i < Constantes.Paginas.size(); i++) {
+            if(Constantes.Paginas.get(i).id == linea.pagina_a_pasar){
+                new_pag = Constantes.Paginas.get(i);
+            }
+        }
+        for (int i = 0; i < Constantes.Frames.size(); i++) {
+            if(Constantes.Frames.get(i).id_pagina == -1){
+                Constantes.Frames.get(i).id_pagina = new_pag.id;
+                Constantes.Frames.get(i).pertenece_a_proceso = new_pag.pertenece_a_proceso;
+            }
+        }
     }
 
     public static void fetch_prepaginacion() {
