03e615cb1963ce0c558545de330d002c05c5497b
Fabian Fdz <fa.fdz@me.com>
2015-05-28 08:47:02 -0600

    Archivo frame  modificado

--- src/Constructores/Frame.java
+++ src/Constructores/Frame.java
@@ -9,11 +9,10 @@ public class Frame {
     public int id,id_pagina,tamaño;
     public String pertenece_a_proceso;
     public Timestamp tiempo;
-    public Pagina pag;
 
     public Frame(int id, int tamaño) {
         this.id = id;
-        this.id_pagina = -1; //-1 quiere decir que no tiene ninguna pagina asignada
+        this.id_pagina = 0; //0 quiere decir que no tiene ninguna pagina asignada
         this.tamaño = tamaño;
         this.pertenece_a_proceso = null;
         this.tiempo = null;
