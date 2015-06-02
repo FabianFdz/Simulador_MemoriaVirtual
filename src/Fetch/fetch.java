f9f174e627115cbb72159741ecc0e8a1ad42a241
Fabian Fdz <fa.fdz@me.com>
2015-06-02 00:42:11 -0600

    Se agrega fetch por prepaginacion

--- src/Fetch/fetch.java
+++ src/Fetch/fetch.java
@@ -21,62 +21,38 @@ import java.util.LinkedList;
 public class fetch {
 
     public static void fetch_prepaginacion(Atributos_text linea, String tipo_reemplazo) {
-        //cada vez que hace una referencia pasa la pagina a memoria
+        //cada vez que hace una referencia pasa la pagina a memoria junto con n paginas más, donde n = working set
+        LinkedList<Pagina> lista_a_memoria = new LinkedList<Pagina>();
         Pagina new_pag = new Pagina(-1, -1, null);
         for (int i = 0; i < Constantes.Paginas.size(); i++) {
             if (Constantes.Paginas.get(i).id == linea.pagina_a_pasar) {
                 new_pag = Constantes.Paginas.get(i);
             }
         }
-        for (int i = 0; i < Constantes.Frames.size(); i++) {
-            if (Constantes.Frames.get(i).id_pagina == 0) {
-                Constantes.Frames.get(i).id_pagina = new_pag.id;
-                Constantes.Frames.get(i).pertenece_a_proceso = new_pag.pertenece_a_proceso;
-            }
-        }
-    }
-
-    /*public static void fetch_demanda(Atributos_text linea, String tipo_reemplazo) {
-        Pagina new_pag = new Pagina(-1, -1, null);
-        for (int i = 0; i < Constantes.Paginas.size(); i++) {
-            if (Constantes.Paginas.get(i).id == linea.pagina_a_pasar) {
-                new_pag = Constantes.Paginas.get(i);
+        LinkedList<Pagina> paginas_proceso = new LinkedList<Pagina>();
+        paginas_proceso = paginas_de_proceso(new_pag.pertenece_a_proceso);
+        int ws = 6; // igualar a variable de configuracion de working set <---------------- OJO!!!!!!
+        for (int i = 0; i < paginas_proceso.size(); i++) {
+            if(paginas_proceso.get(i).id==new_pag.id){
+                for (int j = i; j < paginas_proceso.size(); j++) {
+                    lista_a_memoria.add(paginas_proceso.get(i));
+                    ws = ws--;
+                    if(ws==0){
+                        break;
+                    }
+                    if (j==paginas_proceso.size()-1) {
+                        j = -1;
+                    }
+                }
                 break;
             }
         }
-        if (todos_frames_llenos()) {
-            //aqui lo de reemplazo
-            System.out.println("Todos los frames estan llenos");
-            return;
+        for (int i = 0; i < lista_a_memoria.size(); i++) {
+            linea.id_proceso = lista_a_memoria.get(i).pertenece_a_proceso.nombre;
+            linea.pagina_a_pasar = lista_a_memoria.get(i).id;
+            fetch_demanda(linea, tipo_reemplazo);
         }
-        for (Frame Frame : Constantes.Frames) {
-            if (Frame.id_pagina == 0 && Frame.pertenece_a_proceso.equals(new_pag.pertenece_a_proceso)) {
-                Frame.id_pagina = new_pag.id;
-                Frame.pertenece_a_proceso = new_pag.pertenece_a_proceso;
-                return;
-            }
-        }
-        if (/*Working set variable/true) {
-            if (/*minimo == maximo/true) {
-                working_set.asigna_frame(new_pag.pertenece_a_proceso,/*Aqui working set actual/);
-            } else if (/*minimo + tasa de crecimiento > maximo/true) {
-                working_set.asigna_frame(new_pag.pertenece_a_proceso,/*Aqui working set actual/);
-                //actual = maximo
-            } else {
-                working_set.asigna_frame(new_pag.pertenece_a_proceso,/*Aqui working set actual/);
-                //actual = actual + tasa de crecimiento
-            }
-        } else {
-            working_set.asigna_frame(new_pag.pertenece_a_proceso,/*Aqui working set minimo/);
-        }
-        for (Frame Frame : Constantes.Frames) {
-            if (Frame.id_pagina == 0 && Frame.pertenece_a_proceso.equals(new_pag.pertenece_a_proceso)) {
-                Frame.id_pagina = new_pag.id;
-                Frame.pertenece_a_proceso = new_pag.pertenece_a_proceso;
-                return;
-            }
-        }
-    }*/
+    }
 
     public static void fetch_demanda(Atributos_text linea, String tipo_reemplazo) {
         //cada vez que hace una referencia pasa la pagina a memoria
@@ -165,39 +141,14 @@ public class fetch {
         }
         return true;
     }
+    
+    private static LinkedList<Pagina> paginas_de_proceso(Proceso P){
+        LinkedList <Pagina> res = new LinkedList<Pagina>();
+        for (int i = 0; i < Constantes.Paginas.size(); i++) {
+            if(Constantes.Paginas.get(i).equals(P.nombre)){
+                res.add(Constantes.Paginas.get(i));
+            }
+        }
+        return res;
+    }
 }
-/*
- Cristian
-
- public static void fetch_demanda(Atributos_text linea, String tipo_reemplazo) {
- //cada vez que hace una referencia pasa la pagina a memoria
- Pagina new_pag = new Pagina(-1, -1, null);
- int bandera = 0;
- int paginaDeseada = ((linea.pagina_a_pasar)/(((int) Math.pow(2,constantes.L_Esquema.get(0).getBits_Offset()))/1024));
- if(constantes.Paginas.size() >= paginaDeseada)  {
- if(constantes.Paginas.get(paginaDeseada).pertenece_a_proceso.getId() == Integer.parseInt(linea.id_proceso)){           
- new_pag = constantes.Paginas.get(paginaDeseada);
- bandera = 1;
- }else{
- JOptionPane.showMessageDialog(null, "Error de Carga de referencia","Error al cargar ", JOptionPane.ERROR_MESSAGE);
- bandera = 0;
- }
- }else{
- JOptionPane.showMessageDialog(null, "Error de Carga de referencia","Error al cargar", JOptionPane.ERROR_MESSAGE);
- bandera = 0;
- }
- if (bandera == 1){
- for (int i = 0; i < constantes.Frames.size(); i++) {
- if(constantes.Frames.get(i).id_pagina == 0){
- constantes.Frames.get(i).id_pagina = new_pag.id;
- constantes.Frames.get(i).pertenece_a_proceso = new_pag.pertenece_a_proceso; 
- Frame x = constantes.Frames.get(i);
- System.out.println("Frame hecho" + x.pertenece_a_proceso.getNombre());
- break;
- }
- }
- }
- }
-
-
- */
